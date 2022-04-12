9c4a21b66a2fbe7ea7da5c9caa8cfe3b7d1732a4
biruk <biruklegess@gmail.com>
2022-04-12 05:42:36 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,17 +3,15 @@
  * main - entry block
  * Return: Always 0 (Success)
  **/
-
 int main(void)
 {
-	
-	char biruk[] = "_putchar";
-	
-	int c;
-	
-	for (c=0; c < 8; c++)
+	int n = 0;
+	char str_hbt[9] = "Holberton";
+
+	while (n < 9)
 	{
-		_putchar(biruk[c]);
+		_putchar(str_hbt[n]);
+		n += 1;
 	}
 	_putchar('\n');
 	return (0);
